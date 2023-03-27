3a81b49e733e73828274c2dfe60e263f3b4dbf9e
Caleb <88009118+traitortots@users.noreply.github.com>
2023-03-27 15:34:01 -0400

    add force for nation boundary

--- index.js
+++ index.js
@@ -63,28 +63,39 @@ function map(mapdata) {
       }
     });
 
-  // Add logos for states with teams
-  const logos = svg.selectAll("image")
-    .data(topojson.feature(mapdata, mapdata.objects.states).features)
-    .enter()
-    .filter((d) => d.properties["Has Team"] === "Y")
-    .append("image")
-    .attr("xlink:href", (d) => d.properties.LogoURL)
-    .attr("width", 50)
-    .attr("height", 50)
-    .attr("x", (d) => d3.geoPath().centroid(d)[0] - 25)
-    .attr("y", (d) => d3.geoPath().centroid(d)[1] - 25);
+// Add logos for states with teams
+const logos = svg.selectAll("image")
+.data(topojson.feature(mapdata, mapdata.objects.states).features)
+.enter()
+.filter((d) => d.properties["Has Team"] === "Y")
+.append("image")
+.attr("xlink:href", (d) => d.properties.LogoURL)
+.attr("width", 50)
+.attr("height", 50)
+.attr("x", (d) => d3.geoPath().centroid(d)[0] - 25)
+.attr("y", (d) => d3.geoPath().centroid(d)[1] - 25);
 
-  // Force simulation to prevent overlap
-  const simulation = d3.forceSimulation(logos.data())
-    .force("x", d3.forceX((d) => d3.geoPath().centroid(d)[0]).strength(0.2))
-    .force("y", d3.forceY((d) => d3.geoPath().centroid(d)[1]).strength(0.2))
-    .force("collide", d3.forceCollide(55))
-    .on("tick", () => {
-      logos
-        .attr("x", (d) => d.x - 25)
-        .attr("y", (d) => d.y - 25);
-    });
+// Force simulation to prevent overlap and push logos outside nation geometry
+const simulation = d3.forceSimulation(logos.data())
+.force("x", d3.forceX((d) => d3.geoPath().centroid(d)[0]).strength(0.2))
+.force("y", d3.forceY((d) => d3.geoPath().centroid(d)[1]).strength(0.2))
+.force("collide", d3.forceCollide(55))
+.force("nation", (alpha) => {
+  logos.each(function (d) {
+    const nationGeom = mapdata.objects.nation;
+    const point = { type: "Point", coordinates: [d.x, d.y] };
+    if (d3.geoContains(topojson.feature(mapdata, nationGeom), point)) {
+      const closest = d3.geoInterpolate(point.coordinates, d3.geoPath().centroid(d))(1 + alpha * 0.1);
+      d.x += (closest[0] - d.x) * alpha;
+      d.y += (closest[1] - d.y) * alpha;
+    }
+  });
+})
+.on("tick", () => {
+  logos
+    .attr("x", (d) => d.x - 25)
+    .attr("y", (d) => d.y - 25);
+});
 
 }
 
